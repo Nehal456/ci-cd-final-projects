862d3f89556201991356fc3b58db8cb609a68517
Nehal Akhtar <141544837+Nehal456@users.noreply.github.com>
2025-04-16 01:19:16 +0530

    Update status.py

--- service/common/status.py
+++ service/common/status.py
@@ -6,13 +6,18 @@ Provides common HTTP status codes for the service
 
 class HTTPStatus:
     """Enumeration of HTTP status codes"""
-
+    # 2xx Success
     OK = 200
     CREATED = 201
     ACCEPTED = 202
+    NO_CONTENT = 204
+    
+    # 4xx Client Errors
     BAD_REQUEST = 400
     UNAUTHORIZED = 401
     FORBIDDEN = 403
     NOT_FOUND = 404
     CONFLICT = 409
+    
+    # 5xx Server Errors
     INTERNAL_SERVER_ERROR = 500
